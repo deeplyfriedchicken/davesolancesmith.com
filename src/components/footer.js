fc105ea95d20b66232a35a2872c25d4c7a99d9e9
Kevin <kevin.a.cunanan@gmail.com>
2019-01-06 18:28:59 -0800

    updated footer ui and added react logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import reactLogo from '../assets/react-logo.png';
 import '../styles/footer.css'
 
 class Footer extends Component {
@@ -9,14 +10,12 @@ class Footer extends Component {
         <div className="container has-text-centered">
           <hr/>
           <div className="has-text-centered is-centered">
+            <img href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" className="react-logo" src={reactLogo} alt="react logo" />
             <a href="https://buttercms.com/" target="_blank" rel="noopener noreferrer">
-              <img className="buttercms" alt="buttercms attribution logo" src="https://cdn.buttercms.com/PGJPyIwaQ2KnOA8UyKfH" />
+              <img className="buttercms-logo" alt="buttercms attribution logo" src="https://cdn.buttercms.com/RyJ7UhcVTCRqrCFXwgCo" />
             </a>
           </div>
-          <span className="icon">
-            <i className="fab fa-github"></i>
-          </span>
-          <p>engineered by <a href="https://github.com/kcunanan/davesolancesmith.com" target="_blank" rel="noopener noreferrer">kcunanan</a></p>
+          <p className="copyright">© Dave Smith | Designed by <a href="https://github.com/kcunanan/davesolancesmith.com" target="_blank" rel="noopener noreferrer">Kevin Cunanan <span className="icon"><i className="fab fa-github"></i></span></a></p>
         </div>
       </div>
     )
