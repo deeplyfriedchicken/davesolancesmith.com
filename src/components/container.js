d9da8cf2a1b284078e4fd5dc514d6822f1c49d78
Kevin <kevin.a.cunanan@gmail.com>
2019-01-07 23:37:30 -0800

    fixed some padding issues

--- src/components/container.js
+++ src/components/container.js
@@ -3,15 +3,13 @@ import React, { Component } from 'react'
 class Container extends Component {
   render () {
     return (
-      <div>
-        <div className="container">
-          <div className="columns is-mobile is-multiline is-centered">
-            <div className="column is-10 is-centered">
-              { this.props.children }
-            </div>
-          </div>
+    <div className="container content-container">
+        <div className="columns is-mobile is-multiline is-centered">
+        <div className="column is-10 is-centered">
+            { this.props.children }
         </div>
-      </div>
+        </div>
+    </div>
     )
   }
 }
